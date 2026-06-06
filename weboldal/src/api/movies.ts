64b8d9608b044f081a66cc934d74c152d35fa40c
Becse <kisbecse@gmail.com>
2026-06-06 18:42:37 +0200

    semmi

---
+++ weboldal/src/api/movies.ts
@@ -0,0 +1,24 @@
+import type IMovie from "../interfaces/IMovie";
+
+export async function getMovies() {
+    let rsp = await fetch("/api/movies", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    if (!rsp.ok) {
+        throw new Error("API error: " + rsp.statusText)
+    }
+    return await rsp.json();
+}
+export async function postMovie(movie: IMovie) {
+    let rsp = await fetch("/api/movies", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(movie)
+    })
+}
