54c6b1213379a582c84a7acafbe7442f1c7db819
rnxm <63553775+rnxm@users.noreply.github.com>
2026-06-08 09:23:47 +0200

    added "coming soon" section

--- weboldal/src/index.ts
+++ weboldal/src/index.ts
@@ -2,16 +2,17 @@ import "./main.ts"
 import MovieCarousel from "./components/Carousel/Carousel.mts";
 import MovieGrid from "./components/MovieGrid/MovieGrid.mts";
 import { getMovies } from "./api/movies.ts";
+import type IMovie from "./interfaces/IMovie.ts";
 
 const carousel = document.querySelector("components-moviecarousel") as MovieCarousel;
 const nowPlayingGrid = document.querySelector("#now-playing-grid") as MovieGrid;
 const comingSoonGrid = document.querySelector("#coming-soon-grid") as MovieGrid;
 
-const movies = await getMovies();
+const movies: IMovie[] = await getMovies();
 
 carousel.movies = movies;
 
-const demoGridMovies = [...movies, ...movies, ...movies].slice(0, 14);
+const demoGridMovies = movies.slice(0, 14);
 
 if (nowPlayingGrid) nowPlayingGrid.movies = demoGridMovies;
-if (comingSoonGrid) comingSoonGrid.movies = demoGridMovies;
+if (comingSoonGrid) comingSoonGrid.movies = movies.filter(m => new Date(m.releaseDate) > new Date());
