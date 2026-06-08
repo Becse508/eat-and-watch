6b3b515a2d517c330dc5fa7b130bef9a1a7daab5
Becse <kisbecse@gmail.com>
2026-06-08 02:23:55 +0200

    order delete fixed

---
+++ api/EatAndWatch/EatAndWatch/Migrations/20260608002030_migrans4.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EatAndWatch.Migrations
+{
+    /// <inheritdoc />
+    public partial class migrans4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DeletedAt",
+                table: "Orders",
+                newName: "DeleteTime");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DeleteTime",
+                table: "Orders",
+                newName: "DeletedAt");
+        }
+    }
+}
